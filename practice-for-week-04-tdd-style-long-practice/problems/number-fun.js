b373bb0a9bdb22197334f13163a215f685c84419
michaelluutrong <michaelluutrong@gmail.com>
2023-01-20 14:24:04 -0800

    Phase 2, gitignore

--- practice-for-week-04-tdd-style-long-practice/problems/number-fun.js
+++ practice-for-week-04-tdd-style-long-practice/problems/number-fun.js
@@ -1,12 +1,11 @@
 function returnsThree() {
-  // Your code here
+  return 3
 }
 
 function reciprocal(n) {
-  // Your code here
+  if (n < 1 || n > 1000000) throw new TypeError
+  return (1 / n);
 }
 
-module.exports = {
-  returnsThree,
-  reciprocal
-};
+
+module.exports = { returnsThree, reciprocal }
--- practice-for-week-04-tdd-style-long-practice/test/number-fun-spec.js
+++ practice-for-week-04-tdd-style-long-practice/test/number-fun-spec.js
@@ -1,1 +1,33 @@
-// Your code here
+const chai = require("chai");
+const expect = chai.expect;
+const { returnsThree } = require('../problems/number-fun')
+const { reciprocal } = require('../problems/number-fun')
+
+describe('returnsThree()', () => {
+    it('should return 3', () => {
+        const actual1 = returnsThree();
+        const expected1 = 3;
+        expect(actual1).to.eql(expected1)
+    })
+})
+describe('reciprocal(n)', () => {
+    context("if n is between 1 and 1000000", () => {
+        it('should return the reciprocal', () => {
+            const actual2 = reciprocal(3);
+            const actual4 = reciprocal(100003);
+            const expected2 = (1 / 3);
+            const expected4 = (1 / 100003);
+            expect(actual2).to.eql(expected2)
+            expect(actual4).to.eql(expected4)
+        })
+        it('should work with multiple inputs', () => {
+            expect(reciprocal(3, 5, 7)).to.eql(1 / 3);
+        })
+    })
+    context("if n is not between 1 and 1000000", () => {
+        it('should return a TypeError with a descriptive string message', () => {
+            expect(() => reciprocal(-5)).to.throw(TypeError)
+            expect(() => reciprocal(1000001)).to.throw(TypeError)
+        });
+    })
+})
